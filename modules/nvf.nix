2d975c331fe74b55544b9de47f0b421bb6c53702
Noodl3r <echelon.r6s@gmail.com>
2026-06-27 20:41:08 -0400

    Config fornvf

--- modules/nvf.nix
+++ modules/nvf.nix
@@ -1,14 +1,58 @@
-{ pkgs, ... };
+{ ... }:
 
 {
   programs.nvf = {
     enable = true;
+    # Your settings need to go into the settings attribute set
+    # most settings are documented in the appendix
     settings.vim = {
       lineNumberMode = "relNumber";
-      searchCase = "smart";
-      tabWidth = 2;
-      expandTab = true;
-      scrollOffset = 6;
+      viAlias = true;
+      vimAlias = false;
+      autopairs.nvim-autopairs.enable = true;
+      theme = {
+        enable = true;
+        name = "oxocarbon";
+        style = "dark";
+        transparent = false;
+      };
+      lsp = {
+        enable = true;
+        formatOnSave = true;
+        lspkind.enable = true;
+        trouble.enable = true;
+        otter-nvim.enable = true;
+      };
+      debugger = {
+        nvim-dap = {
+          enable = true;
+          ui.enable  = true;
+        };
+      };
+      languages = {
+        enableFormat = true;
+        enableTreesitter = true;
+        enableExtraDiagnostics = true;
+
+        nix.enable = true;
+        markdown.enable = true;
+        clang.enable = true;
+        cmake.enable = true;
+        java.enable = true;
+        python.enable = true;
+        rust.enable = true;
+        docker.enable = true;
+        lua.enable = true;
+      };
+      autocomplete = {
+        blink-cmp.enable = true;
+      };
+      fzf-lua = {
+        enable = true;
+        profile = "borderless-full";
+      };
+      utility.oil-nvim = {
+        enable = true;
+        gitStatus.enable = true;
     };
-  };
 }
