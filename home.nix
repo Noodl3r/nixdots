e4f83d40651b133b897b4a4758a25ea4e8c41a43
Noodl3r <echelon.r6s@gmail.com>
2026-07-12 15:47:45 -0400

    Geting rid of all the ocaml entry messages

--- home.nix
+++ home.nix
@@ -3,70 +3,132 @@
   pkgs,
   ...
 }: {
-  home.username = "noodl3";
-  home.homeDirectory = "/home/noodl3";
-  home.stateVersion = "25.05";
-
-  programs.fish = {
-    enable = true;
-    interactiveShellInit = ''
-      set fish_greeting
-      fish_add_path ~/.local/bin
-      set -g fish_key_bindings fish_vi_key_bindings
-      set -f fish_cursor_default block blink
-      set -f fish_cursor_insert line blink
-      set -f fish_cursor_replace_one underscore blink
-      set -f fish_cursor_visual block
-      if test -r "$HOME/.opam/opam-init/init.fish"
-        source "$HOME/.opam/opam-init/init.fish" > /dev/null 2> /dev/null
-      end
-    '';
-
-    shellAliases = {
-      btw = "echo I use nixos btw";
-      v = "nvim";
-      rebuild = "sudo nixos-rebuild switch --flake ~/nixos-dotfiles#multivac --no-reexec";
-      check = "nix flake check ~/nixos-dotfiles";
-      lg = "lazygit";
+  home = {
+    username = "noodl3";
+    homeDirectory = "/home/noodl3";
+    stateVersion = "25.05";
+    sessionVariables = {
+      DIRENV_LOG_FORMAT = "";
+    };
+    file = {
+      ".local/bin".source = ./scripts;
+      ".config/i3".source = ./config/i3;
+      ".config/i3status".source = ./config/i3status;
+      ".config/kitty".source = ./config/kitty;
+      ".config/picom".source = ./config/picom;
+      ".vimrc".source = ./config/vim/vimrc;
     };
   };
+  programs = {
+    fish = {
+      enable = true;
+      interactiveShellInit = ''
+        set fish_greeting
+        fish_add_path ~/.local/bin
+        set -g fish_key_bindings fish_vi_key_bindings
+        set -f fish_cursor_default block blink
+        set -f fish_cursor_insert line blink
+        set -f fish_cursor_replace_one underscore blink
+        set -f fish_cursor_visual block
+        set -gx DIRENV_LOG_FORMAT ""
+      '';
 
-  programs.zoxide = {
-    enable = true;
-    enableFishIntegration = true;
-    options = ["--cmd cd"];
-  };
-
-  programs.direnv = {
-    enable = true;
-    nix-direnv.enable = true;
-    config = {
-      global = {
-        # Hides block
-        hide_env_diff = true;
-        # Silence everything
-        # log_format = "-";
+      shellAliases = {
+        btw = "echo I use nixos btw";
+        v = "nvim";
+        rebuild = "sudo nixos-rebuild switch --flake ~/nixos-dotfiles#multivac --no-reexec";
+        check = "nix flake check ~/nixos-dotfiles";
+        lg = "lazygit";
       };
     };
-  };
 
-  programs.starship = {
-    enable = true;
-    enableFishIntegration = true;
-    settings = {
-      add_newline = true;
-      character = {
-        success_symbol = "[ξ](bold cyan)";
-        error_symbol = "[ξ](bold red)";
-        vimcmd_symbol = "[ξ](bold cyan)";
+    zoxide = {
+      enable = true;
+      enableFishIntegration = true;
+      options = ["--cmd cd"];
+    };
+
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+      config = {
+        global = {
+          # Hides block
+          hide_env_diff = true;
+          # Silence everything
+          # log_format = "-";
+        };
       };
     };
-  };
 
-  programs.git = {
-    enable = true;
-    settings.user.name = "Noodl3r";
-    settings.user.email = "echelon.r6s@gmail.com";
+    starship = {
+      enable = true;
+      enableFishIntegration = true;
+      settings = {
+        add_newline = true;
+        character = {
+          success_symbol = "[ξ](bold cyan)";
+          error_symbol = "[ξ](bold red)";
+          vimcmd_symbol = "[ξ](bold cyan)";
+        };
+      };
+    };
+
+    git = {
+      enable = true;
+      settings.user.name = "Noodl3r";
+      settings.user.email = "echelon.r6s@gmail.com";
+    };
+
+    tmux = {
+      enable = true;
+      shell = "${pkgs.fish}/bin/fish";
+      clock24 = true;
+      keyMode = "vi";
+      terminal = "tmux-256color";
+      shortcut = "a";
+      baseIndex = 1;
+      mouse = true;
+      plugins = with pkgs.tmuxPlugins; [
+        sensible
+        vim-tmux-navigator
+        resurrect
+        continuum
+      ];
+      # must be a better way to do this lol
+      extraConfig = ''
+        set -g status-position top
+        set -g status on
+        set -g status-left ""
+        set -g status-right ""
+        set -g status-style bg=#000000,fg=#000000
+        set -g window-status-style fg=#6c7086,bg=#1e1e2e
+        set -g window-status-current-style fg=#ff00ff,bold,bg=#000000
+        set -g window-status-separator ""
+        set -g window-status-format " #I "
+        set -g window-status-current-format " #I "
+        bind-key h select-pane -L
+        bind-key j select-pane -D
+        bind-key k select-pane -U
+        bind-key l select-pane -R
+
+        set-option -g set-clipboard on
+        bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "xclip -selection clipboard -in"
+        bind-key e display-popup -E -w 80% -h 80% "ts"
+      '';
+    };
+
+    firefox = {
+      enable = true;
+      configPath = ".mozilla/firefox";
+      profiles.default = {
+        settings = {
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          "browser.compactmode.show" = true;
+        };
+        userChrome = builtins.readFile ./config/firefox/userChrome.css;
+      };
+    };
   };
 
   services.screen-locker = {
@@ -75,61 +137,4 @@
     inactiveInterval = 5;
     xautolock.enable = true;
   };
-
-  programs.tmux = {
-    enable = true;
-    shell = "${pkgs.fish}/bin/fish";
-    clock24 = true;
-    keyMode = "vi";
-    terminal = "tmux-256color";
-    shortcut = "a";
-    baseIndex = 1;
-    mouse = true;
-    plugins = with pkgs.tmuxPlugins; [
-      sensible
-      vim-tmux-navigator
-      resurrect
-      continuum
-    ];
-    # must be a better way to do this lol
-    extraConfig = ''
-      set -g status-position top
-      set -g status on
-      set -g status-left ""
-      set -g status-right ""
-      set -g status-style bg=#000000,fg=#000000
-      set -g window-status-style fg=#6c7086,bg=#1e1e2e
-      set -g window-status-current-style fg=#ff00ff,bold,bg=#000000
-      set -g window-status-separator ""
-      set -g window-status-format " #I "
-      set -g window-status-current-format " #I "
-      bind-key h select-pane -L
-      bind-key j select-pane -D
-      bind-key k select-pane -U
-      bind-key l select-pane -R
-
-      set-option -g set-clipboard on
-      bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "xclip -selection clipboard -in"
-      bind-key e display-popup -E -w 80% -h 80% "ts"
-    '';
-  };
-
-  programs.firefox = {
-    enable = true;
-    configPath = ".mozilla/firefox";
-    profiles.default = {
-      settings = {
-        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-        "browser.compactmode.show" = true;
-      };
-      userChrome = builtins.readFile ./config/firefox/userChrome.css;
-    };
-  };
-
-  home.file.".local/bin".source = ./scripts;
-  home.file.".config/i3".source = ./config/i3;
-  home.file.".config/i3status".source = ./config/i3status;
-  home.file.".config/kitty".source = ./config/kitty;
-  home.file.".config/picom".source = ./config/picom;
-  home.file.".vimrc".source = ./config/vim/vimrc;
 }
