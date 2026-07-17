4dfc2576a4b636296215e161d672149bafb7f2c3
Noodl3r <echelon.r6s@gmail.com>
2026-07-16 22:54:58 -0400

    More fixes

--- home.nix
+++ home.nix
@@ -7,30 +7,9 @@
     ./home/core.nix
     ./home/files.nix
     ./home/services.nix
+    ./home/programs/fish.nix
   ];
   programs = {
-    fish = {
-      enable = true;
-      interactiveShellInit = ''
-        set fish_greeting
-        fish_add_path ~/.local/bin
-        set -g fish_key_bindings fish_vi_key_bindings
-        set -f fish_cursor_default block blink
-        set -f fish_cursor_insert line blink
-        set -f fish_cursor_replace_one underscore blink
-        set -f fish_cursor_visual block
-        set -gx DIRENV_LOG_FORMAT ""
-      '';
-
-      shellAliases = {
-        btw = "echo I use nixos btw";
-        v = "nvim";
-        rebuild = "sudo nixos-rebuild switch --flake ~/dotfiles#multivac --no-reexec";
-        check = "nix flake check ~/dotfiles";
-        lg = "lazygit";
-      };
-    };
-
     zoxide = {
       enable = true;
       enableFishIntegration = true;
