15687b47bb341d04568c35bebfd2942ae8b259e4
Noodl3r <echelon.r6s@gmail.com>
2026-08-03 09:20:24 -0400

    Most recent changes to the dev-init scripts

--- home/services.nix
+++ home/services.nix
@@ -2,7 +2,7 @@
   services.screen-locker = {
     enable = true;
     lockCmd = "${pkgs.i3lock}/bin/i3lock -n -c 000000";
-    inactiveInterval = 5;
+    inactiveInterval = 50;
     xautolock.enable = true;
   };
 }
